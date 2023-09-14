41d2ae17685024ca8e1e8d6c233adaccbbea7bcb
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-13 20:52:32 -0400

    cheese app changes

---
+++ controllers/cheeseController.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router()
+const Cheese = require('../models/Cheese')
+
+// INDEX
+router.get('/', async (req, res) => {
+    try {
+        res.json(await Cheese.find())
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+// CREATE
+router.post('/', async (req, res) => {
+    try {
+        res.json(await Cheese.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        res.json(await Cheese.findByIdAndDelete(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+// UPDATE
+router.put('/:id', async (req, res) => {
+    try {
+        res.json(await Cheese.findByIdAndUpdate(req.params.id, req.body, {new :true}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+// SHOW
+router.get('/:id', async (req, res) => {
+    try {
+        res.json(await Cheese.findById(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+require('dotenv').config()
+const cheeseController = require('./controllers/cheeseController')
+const morgan = require('morgan')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('tiny'))
+app.use('/cheese', cheeseController)
+
+app.get('/', (req, res) => {
+    res.send("Hello world!")
+})
+
+app.listen(PORT,() => console.log(`Cheese-App on ${PORT}`))
